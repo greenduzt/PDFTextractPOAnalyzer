e11425937ee7846036199dae28b610646a2a62ed
chamara <chamara_sachintha@yahoo.com>
2024-05-02 13:49:07 +1000

    Initial project upload

---
+++ PDFTextractPOAnalyzer/Program.cs
@@ -0,0 +1,16 @@
+﻿using Amazon;
+using Microsoft.Extensions.Configuration;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var region = RegionEndpoint.APSoutheast2;
+
+        //Set up the config to load the user secrets
+        IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddUserSecrets<Program>(true)
+            .Build();
+    }
+}
